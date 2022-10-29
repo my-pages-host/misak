4100c674bedf59a47951246144da5bc98fdbf102
Simon Podlipsky <simon@podlipsky.net>
2022-10-29 23:11:50 +0200

    a bit of responsivity

--- src/Cover.tsx
+++ src/Cover.tsx
@@ -1,15 +1,15 @@
-import styles from "./Cover.module.css";
+import styles from "./Cover.module.scss";
 import clsx from "clsx";
 
 export const Cover = () => <div className={clsx(styles.container, "site-blocks-cover")} id="home-section">
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-md-12 mt-lg-5 text-left align-self-center text-intro">
+      <div className="col-md-12 mt-lg-5 text-left align-self-end align-self-md-center text-intro">
         <div className="row">
           <div className="col-lg-6">
             <h1 className="text-black">Míša Podlipská</h1>
             <p className="lead">Delivery garant of business value stream</p>
-            <p>
+            <p className="d-none d-md-block">
               <a href="#portfolio-section" className="btn smoothscroll btn-primary">For your business I can do</a>
             </p>
           </div>
