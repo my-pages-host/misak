8664f117f1c415ab994ae5528db8f8bbca26fcab
my-pages-host <my-pages-host@users.noreply.github.com>
2022-10-29 18:57:31 +0000

    deploy: 095ede37f424395751d163c11460d01582e31891

---
+++ _next/static/Vytc508BeYGdhCDuo-pkK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2d5b32585c929f90.css","static/chunks/pages/index-798bcc5b73283ba9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
