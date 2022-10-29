d19c81f4ee7c1a4b93f3959fca16c742a7cad7eb
Simon Podlipsky <simon@podlipsky.net>
2022-10-29 20:56:48 +0200

    remove bootstrap vol.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import "../styles/globals.css";
 import "../styles/font-face.css";
 import "../styles/old.css";
-import 'bootstrap/dist/css/bootstrap.css'
 
 const MyApp = ({ Component, pageProps }) => <Component {... pageProps} />;
 
