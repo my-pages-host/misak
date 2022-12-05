1cefd9eaf4285352ca56f6458a9ec779619d01b6
my-pages-host <116964476+my-pages-host@users.noreply.github.com>
2022-12-05 22:01:13 +0000

    Deploying to gh-pages from @ my-pages-host/misak@8cb1336b2d3e2471332d7db8770ee8754681717f 🚀

---
+++ _next/static/zrVBuSDsVGrYy-iBJmc_V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/78f1e14ddfe3eb60.css","static/chunks/pages/index-00a8751246112d0c.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
