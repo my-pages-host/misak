9d146093a23e85f30dcf25afb1b04be3760d0735
Simon Podlipsky <simon@podlipsky.net>
2022-12-09 20:26:57 +0100

    add hotjar tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,19 @@ const MyApp = ({ Component, pageProps }) =>
       {`let vh = window.innerHeight * 0.01;`}
       {"document.documentElement.style.setProperty('--vh', `${vh}px`);"}
     </Script>
+    <Script id="hotjar">
+      {`(
+      function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3282913,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        }
+        )(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+    </Script>
     <Component {... pageProps} />
   </>;
 
