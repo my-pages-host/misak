e7a663f588539a3b10d46909e24eb6b682d935fc
Simon Podlipsky <simon@podlipsky.net>
2022-12-09 13:37:31 +0100

    fix 100vh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,15 @@ import "../styles/globals.css";
 import "../styles/font-face.css";
 import "../styles/styles.scss";
 import "../styles/old.scss";
+import Script from "next/script";
 
-const MyApp = ({ Component, pageProps }) => <Component {... pageProps} />;
+const MyApp = ({ Component, pageProps }) =>
+  <>
+    <Script id="vh">
+      {`let vh = window.innerHeight * 0.01;`}
+      {"document.documentElement.style.setProperty('--vh', `${vh}px`);"}
+    </Script>
+    <Component {... pageProps} />
+  </>;
 
 export default MyApp;
