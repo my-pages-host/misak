c35984a9efc26bf0b98e58921676f5b84ae7491d
my-pages-host <116964476+my-pages-host@users.noreply.github.com>
2023-01-28 20:49:52 +0000

    Deploying to gh-pages from @ my-pages-host/misak@1040d224162b5ac16b3d0f87379aedfc785ea9b8 🚀

---
+++ _next/static/XUw2cpp2yK5FZ8N9S7_Lq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e71c32bb7f404e53.css","static/chunks/pages/index-b927abbe731efa4e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/umKxNetWHgBBwh7lqSR54/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e71c32bb7f404e53.css","static/chunks/pages/index-1f39a8029d0e1bc1.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
